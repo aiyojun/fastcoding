8cca972988890a8c0a679c725ba3272496c5b6e4
DJun <32353521+aiyojun@users.noreply.github.com>
2022-10-25 13:59:38 +0800

    Update noderestfulserver.js

--- httpserver/noderestfulserver.js
+++ httpserver/noderestfulserver.js
@@ -4,8 +4,12 @@ const serveStatic = require('serve-static')
 
 class RestfulServer {
     constructor(listeners = [], staticPath = './public') {
+        this.staticPath = staticPath
         for (let i = 0; i < listeners.length; i++) {
-            this.restfulHandlers.push(listeners[i])
+            const handler = listeners[i]
+            const iMethod = handler.method
+            const iUrl    = handler.url
+            this.restfulHandlers.set(iUrl, {handler: listeners[i], method: iMethod.toLowerCase()})
         }
         this.server = http.createServer((request, response) => {
             const method = request.method.toLowerCase()
@@ -17,40 +21,42 @@ class RestfulServer {
             for (let k in query) {
                 parameters[k] = query[k]
             }
-
             console.info(`[Rest] http request, method ${method}, path ${url} `)
-            for (let i = 0; i < this.restfulHandlers.length; i++) {
-                const handler = this.restfulHandlers[i]
-                const iMethod = handler.method
-                const iUrl    = handler.url
-                if (iMethod.toLowerCase() === method && iUrl === url) {
-                    let body = []
-                    request.on('data', (chunk) => {
-                        body.push(chunk)
-                    })
-                    request.on('end', () => {
-                        body = Buffer.concat(body).toString('utf8')
-                        try {
-                            const obj = handler.onSuccess({method: method, headers, parameters, data: body})
-                            if (typeof obj === 'object') {
-                                response.end(JSON.stringify(obj), 'utf8')
-                            } else if (typeof obj === 'string') {
-                                response.end(obj, 'utf8')
-                            } else {
-                                response.statusCode = 500
-                                response.statusMessage = `typeof return ${typeof obj}`
-                                response.end()
-                            }
-                        } catch (e) {
-                            response.statusCode = 500
-                            response.statusMessage = `internal error: ${e.message}`
-                            response.end()
-                        }
-                    })
-                    return
+            this._process(method, url, request, response, headers, parameters)
+        })
+    }
+
+    _process(method, url, request, response, headers, parameters) {
+        if (this.restfulHandlers.has(url) && this.restfulHandlers.get(url).method === method) {
+            const handler = this.restfulHandlers.get(url).handler
+            let body = []
+            request.on('data', (chunk) => {
+                body.push(chunk)
+            })
+            request.on('end', () => {
+                body = Buffer.concat(body).toString('utf8')
+                try {
+                    const obj = handler.onSuccess({method: method, headers, parameters, data: body}, request, response, this)
+                    if (obj === undefined || obj === null) {
+                        return
+                    }
+                    if (typeof obj === 'object') {
+                        response.end(JSON.stringify(obj), 'utf8')
+                    } else if (typeof obj === 'string') {
+                        response.end(obj, 'utf8')
+                    } else {
+                        response.statusCode = 500
+                        response.statusMessage = `typeof return ${typeof obj}`
+                        response.end()
+                    }
+                } catch (e) {
+                    response.statusCode = 500
+                    response.statusMessage = `internal error: ${e.message}`
+                    response.end()
                 }
-            }
-            const serve = serveStatic(staticPath)
+            })
+        } else {
+            const serve = serveStatic(this.staticPath)
             serve(request, response, function () {
                 const err = `[Rest] server occur error, method: ${request.method.toLowerCase()}, url: ${request.url}!`
                 console.info(err)
@@ -58,18 +64,33 @@ class RestfulServer {
                 response.statusMessage = err
                 response.end(err, 'utf8')
             });
-        })
+        }
     }
 
-    start({port, staticPath}) {
-        this.server.listen(port, staticPath)
+    redirect(url, request, response) {
+        console.info(`[rest] redirect ${request.url} => ${url}`)
+        request.url = url
+        const method = request.method.toLowerCase()
+        const { headers } = request
+        const parsedUrl = urlParser.parse(request.url, true)
+        const query = parsedUrl.query
+        const parameters = {}
+        for (let k in query) {
+            parameters[k] = query[k]
+        }
+        this._process(method, url, request, response, headers, parameters)
+    }
+
+    start(port) {
+        console.info(`[rest] rest service listen on: ${port}`)
+        this.server.listen(port)
     }
 
     errorHandler = (request, response) => {
         return 'error'
     }
 
-    restfulHandlers = []
+    restfulHandlers = new Map
 }
 
 const restfulServer = new RestfulServer([
